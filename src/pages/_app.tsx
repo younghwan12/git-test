60f9099af82e6593ab4a6eb2e636fb0b7a884177
lyh <dowk3432@gmail.com>
2024-01-11 15:37:53 +0900

    test 배포 시작

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-
+// 여기는 테스트입니다.
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
